a936b485c1b2341a1dbd6b3bf5bfc2712f7c4963
Cameron Jacobson <cameron@datashovel.com>
2013-04-08 03:13:26 +0000

    moved some bootstrap into Mestizo class

--- examples/example.php
+++ examples/example.php
@@ -2,46 +2,14 @@
 
 require_once(dirname(__DIR__).'/vendor/autoload.php');
 
-use Doctrine\ORM\Tools\Setup;
-use Doctrine\ORM\EntityManager;
 use Mestizo\Mestizo;
 use Mestizo\Models\Setting;
-
-$paths = array(
-	dirname(__DIR__).'/src/Mestizo/Models/Directory.php',
-	dirname(__DIR__).'/src/Mestizo/Models/Filter.php',
-	dirname(__DIR__).'/src/Mestizo/Models/Handler.php',
-	dirname(__DIR__).'/src/Mestizo/Models/Host.php',
-	dirname(__DIR__).'/src/Mestizo/Models/Log.php',
-	dirname(__DIR__).'/src/Mestizo/Models/Mimetype.php',
-	dirname(__DIR__).'/src/Mestizo/Models/Proxy.php',
-	dirname(__DIR__).'/src/Mestizo/Models/Route.php',
-	dirname(__DIR__).'/src/Mestizo/Models/Server.php',
-	dirname(__DIR__).'/src/Mestizo/Models/Setting.php',
-	dirname(__DIR__).'/src/Mestizo/Models/Statistic.php'
-);
-$isDevMode = false;
-
-$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
-$config->setSQLLogger(new \Doctrine\DBAL\Logging\EchoSQLLogger());
-$em = EntityManager::create(array(
-	//'user'=>'',
-	//'password'=>'',
-	'path'=>dirname(__DIR__).'/config/mestizo.db',
-	//'memory'=>'false',
-	'driver'=>'pdo_sqlite'
-), $config);
 
 $setting = new Setting();
 $setting->key = 'key';
 $setting->value = 'value';
-$em->persist($setting);
-$em->flush();
 
-$em->remove($setting);
-$em->flush();
+$mestizo = new Mestizo();
 
-var_dump($setting);
-//$mestizo = new Mestizo($em);
-
-
+$mestizo->persist($setting);
+$mestizo->remove($setting);
--- src/Mestizo/Mestizo.php
+++ src/Mestizo/Mestizo.php
@@ -2,11 +2,44 @@
 
 namespace Mestizo;
 
+use \Doctrine\ORM\Tools\Setup;
 use \Doctrine\ORM\EntityManager;
 use \Exception;
+use \DirectoryIterator;
 
 class Mestizo
 {
-	public function __construct(EntityManager $em){
+	private $em;
+
+	public function __construct(){
+
+		$models = array();
+		$dir = new DirectoryIterator(__DIR__.'/Models');
+		foreach($dir as $file){
+			if($file->isFile()){
+				$models[] = $file->getPathname();
+			}
+		}
+
+		$devMode = false;
+		$config = Setup::createAnnotationMetadataConfiguration($models, $devMode);
+		//$config->setSQLLogger(new \Doctrine\DBAL\Logging\EchoSQLLogger());
+		$this->em = EntityManager::create(array(
+		    //'user'=>'',
+		    //'password'=>'',
+		    'path'=>dirname(__DIR__).'/../config/mestizo.db',
+		    //'memory'=>'false',
+		    'driver'=>'pdo_sqlite'
+		), $config);
+	}
+
+	public function persist($obj){
+		$this->em->persist($obj);
+		$this->em->flush();
+	}
+
+	public function remove($obj){
+		$this->em->remove($obj);
+		$this->em->flush();
 	}
 }
