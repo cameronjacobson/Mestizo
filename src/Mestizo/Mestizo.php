66a6b9c6a6b15e712a4cef8df45505ed13c022ee
Cameron Jacobson <cameron@datashovel.com>
2013-04-08 00:10:33 +0000

    progress

--- src/Mestizo/Mestizo.php
+++ src/Mestizo/Mestizo.php
@@ -5,10 +5,16 @@ namespace Mestizo;
 use \PDO;
 use \PDOStatement;
 use \PDOException;
+use \Exception;
 
 class Mestizo
 {
 	public function __construct(){
+		$dbfile = dirname(__DIR__).'/../config/mestizo.db';
+		if(!file_exists($dbfile)){
+			throw new Exception('Database not found.  Run bin/initdb to proceed.');
+		}
+
 		
 	}
 }
